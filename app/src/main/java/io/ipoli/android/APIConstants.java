a8d78930e1b3d4fe591e09d5c112ba43260c12c5
jackiezjiang <jackiezjiang@gmail.com>
2016-10-20 18:57:21 -0400

    Delete APIConstants.java

--- app/src/main/java/io/ipoli/android/APIConstants.java
+++
@@ -1,7 +0,0 @@
-public interface APIConstants {
-    String DEV_IPOLI_ENDPOINT = "http://10.0.3.2/v1/";
-    String PROD_IPOLI_ENDPOINT = "http://10.0.3.2/v1/";
-    String DEV_SCHEDULING_ENDPOINT = "http://10.0.3.2/v1/";
-    String PROD_SCHEDULING_ENDPOINT = "http://10.0.3.2/v1/";
-    String API_KEY = "test";
-}
