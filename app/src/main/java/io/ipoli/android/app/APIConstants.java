c75779cc85d429072dec7e72837c6e3496d8a894
jackiezjiang <jackiezjiang@gmail.com>
2016-10-20 18:54:13 -0400

    Create APIConstants.java

---
+++ app/src/main/java/io/ipoli/android/app/APIConstants.java
@@ -0,0 +1,7 @@
+public interface APIConstants {
+    String DEV_IPOLI_ENDPOINT = "http://10.0.3.2/v1/";
+    String PROD_IPOLI_ENDPOINT = "http://10.0.3.2/v1/";
+    String DEV_SCHEDULING_ENDPOINT = "http://10.0.3.2/v1/";
+    String PROD_SCHEDULING_ENDPOINT = "http://10.0.3.2/v1/";
+    String API_KEY = "test";
+}
