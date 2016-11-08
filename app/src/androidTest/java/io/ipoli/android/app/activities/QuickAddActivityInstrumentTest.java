6c76a29cc24b07b0496dd4aabc1696f6004ec8a4
jackiezjiang <jackiezjiang@gmail.com>
2016-11-08 18:35:34 -0500

    Create QuickAddActivityInstrumentTest.java

---
+++ app/src/androidTest/java/io/ipoli/android/app/activities/QuickAddActivityInstrumentTest.java
@@ -0,0 +1,40 @@
+package io.ipoli.android.app.activities;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.ipoli.android.Constants;
+import io.ipoli.android.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by YZ on 11/8/16.
+ */
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class QuickAddActivityInstrumentTest {
+
+    @Rule
+    public ActivityTestRule<QuickAddActivity> quickAddActivityActivityTestRule = new ActivityTestRule<QuickAddActivity>(QuickAddActivity.class);
+
+
+
+    @Test
+    public void testCancel() {
+        onView(withId(R.id.quick_add_text)).perform(click());
+
+    }
+
+    
+
+
+}
