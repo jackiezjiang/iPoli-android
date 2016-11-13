c27f661fecc9c65ab20b595cb3fc292947ed5f25
jackiezjiang <jackiezjiang@gmail.com>
2016-11-12 23:38:44 -0500

    Update QuickAddActivityInstrumentTest.java

--- app/src/androidTest/java/io/ipoli/android/app/activities/QuickAddActivityInstrumentTest.java
+++ app/src/androidTest/java/io/ipoli/android/app/activities/QuickAddActivityInstrumentTest.java
@@ -1,3 +1,24 @@
+package io.ipoli.android.app.activities;
+
+import android.app.Instrumentation;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.ipoli.android.Constants;
+import io.ipoli.android.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 public class QuickAddActivityInstrumentTest extends ActivityInstrumentationTestCase2<QuickAddActivity> {
     private QuickAddActivity quickAddActivity;
