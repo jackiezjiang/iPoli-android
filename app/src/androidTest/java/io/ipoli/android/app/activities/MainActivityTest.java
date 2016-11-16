66f127934f2b4e5d5ff95f9f5bee97fbcddf6313
jackiezjiang <jackiezjiang@gmail.com>
2016-11-16 15:27:50 -0500

    Create MainActivityTest.java

---
+++ app/src/androidTest/java/io/ipoli/android/app/activities/MainActivityTest.java
@@ -0,0 +1,106 @@
+package io.ipoli.android;
+
+import android.app.Fragment;
+import android.app.Instrumentation;
+import android.content.ClipData;
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TextInputEditText;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.widget.DrawerLayout;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.test.ViewAsserts;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.robotium.solo.Solo;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.ipoli.android.app.activities.QuickAddActivity;
+import io.ipoli.android.quest.activities.EditQuestActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.doubleClick;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.flurry.sdk.kp.a.f;
+
+
+/**
+ * Created by YZ on 11/8/16.
+ */
+@LargeTest
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    public static final String ACTION_ADD_QUEST_FROM_WIDGET = "io.ipoli.android.intent.action.ADD_QUEST_FROM_WIDGET";
+    private Solo solo;
+    private MainActivity mMainActivity;
+    private Instrumentation mInstrumentation;
+    private NavigationView navigationView;
+    private View contentContainer;
+    private View quickAdd;
+
+
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        setActivityInitialTouchMode(true);
+        mInstrumentation = getInstrumentation();
+        super.setUp();
+        mMainActivity = getActivity();
+
+        contentContainer = mMainActivity.findViewById(R.id.content_container );
+        quickAdd = mMainActivity.findViewById(R.id.add_quest);
+        navigationView = mMainActivity.navigationView;
+
+    }
+
+
+    @Test
+    public void test1() {
+        View mainActivityDecorView = mMainActivity.getWindow().getDecorView();
+        ViewAsserts.assertOnScreen(mainActivityDecorView, contentContainer);
+
+        Instrumentation.ActivityMonitor searchActivityMonitor =
+                mInstrumentation.addMonitor(QuickAddActivity.class.getName(),
+                        null, false);
+
+
+
+
+
+
+        FloatingActionButton quest = (FloatingActionButton) mMainActivity.findViewById(R.id.add_quest);
+
+
+        quest.callOnClick();
+
+
+
+
+        //MenuItem challenge = navigationView.getMenu().findItem(R.id.challenges);
+
+    }
+
+
+
+
+
+
+}
