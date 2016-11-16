4c18cde16cd671a9ad3d8e89e306be52b0b8eb43
jackiezjiang <jackiezjiang@gmail.com>
2016-11-16 15:06:46 -0500

    Update QuickAddActivityInstrumentTest.java

--- app/src/androidTest/java/io/ipoli/android/app/activities/QuickAddActivityInstrumentTest.java
+++ app/src/androidTest/java/io/ipoli/android/app/activities/QuickAddActivityInstrumentTest.java
@@ -1,12 +1,14 @@
 package io.ipoli.android.app.activities;
 
 import android.app.Instrumentation;
+import android.support.design.widget.TextInputEditText;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
 import android.widget.Button;
-
+import io.ipoli.android.quest.data.Quest;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,20 +16,28 @@ import org.junit.runner.RunWith;
 import io.ipoli.android.Constants;
 import io.ipoli.android.R;
 
+
+import static android.content.Intent.getIntentOld;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
+/**
+ * Created by YZ on 11/8/16.
+ */
+
+@LargeTest
+
 public class QuickAddActivityInstrumentTest extends ActivityInstrumentationTestCase2<QuickAddActivity> {
     private QuickAddActivity quickAddActivity;
     private Instrumentation mInstrumentation;
     private TextInputEditText textInputEditText;
-    
     public QuickAddActivityInstrumentTest() {
         super(QuickAddActivity.class);
     }
+
 
     @Override
     public void setUp() throws Exception {
@@ -35,26 +45,20 @@ public class QuickAddActivityInstrumentTest extends ActivityInstrumentationTestC
         mInstrumentation = getInstrumentation();
         super.setUp();
         quickAddActivity = getActivity();
-
-
-         Button button=  (Button) quickAddActivity.findViewById(R.id.cancel);
-          textInputEditText = (TextInputEditText) quickAddActivity.findViewById(R.id.quick_add_text);
+        textInputEditText = (TextInputEditText) quickAddActivity.findViewById(R.id.quick_add_text);
 
 
 
 
     }
 
-   
     @Test
     public void testAdd() {
-        String additionalText = "TEST";
+
         Button button=  (Button) quickAddActivity.findViewById(R.id.add);
         pressImeActionButton();
-        
-        
-        
-         final String text = "TEST" + additionalText;
+        String additionalText =   textInputEditText.getEditableText().toString();
+        final String text = "TEST" + additionalText;
         quickAddActivity.runOnUiThread(new Runnable() {
 
             @Override
@@ -65,10 +69,32 @@ public class QuickAddActivityInstrumentTest extends ActivityInstrumentationTestC
 
         });
 
+       //textInputEditText.setText(additionalText);
+
+
 
     }
 
 
+
+    private  void updateText(final TextInputEditText textInputEditText, final String value) {
+        try {
+
+            quickAddActivity.runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    textInputEditText.setText(value);
+                }
+
+
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
+
+    }
 
     @Test
     public void testCancel() {
@@ -78,6 +104,8 @@ public class QuickAddActivityInstrumentTest extends ActivityInstrumentationTestC
 
 
     }
+
+
 
 
 
