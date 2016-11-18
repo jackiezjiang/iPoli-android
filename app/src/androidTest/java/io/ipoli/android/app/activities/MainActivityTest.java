892690de3fa54aed7f876baaa2bbe327caab6ebe
jackiezjiang <jackiezjiang@gmail.com>
2016-11-18 16:22:48 -0500

    Update MainActivityTest.java

--- app/src/androidTest/java/io/ipoli/android/app/activities/MainActivityTest.java
+++ app/src/androidTest/java/io/ipoli/android/app/activities/MainActivityTest.java
@@ -15,6 +15,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
 import android.test.ViewAsserts;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +31,7 @@ import org.junit.runner.RunWith;
 
 import io.ipoli.android.app.activities.QuickAddActivity;
 import io.ipoli.android.quest.activities.EditQuestActivity;
+import io.ipoli.android.quest.events.EditQuestRequestEvent;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -54,6 +56,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     private Instrumentation mInstrumentation;
     private NavigationView navigationView;
     private View contentContainer;
+    private DrawerLayout drawerLayout;
 
 
 
@@ -68,17 +71,18 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         mInstrumentation = getInstrumentation();
         super.setUp();
         mMainActivity = getActivity();
-
+        solo = new Solo(mInstrumentation, mMainActivity);
         contentContainer = mMainActivity.findViewById(R.id.content_container );
+        drawerLayout = (DrawerLayout) mMainActivity.findViewById(R.id.drawer_layout);
 
         navigationView = (NavigationView) mMainActivity.findViewById(R.id.navigation_view);
 
     }
-
+/*
 
     @Test
     public void testAddButton() {
-        mMainActivity = getActivity();
+
         FloatingActionButton quickAdd = (FloatingActionButton) mMainActivity.findViewById(R.id.add_quest);
         mMainActivity.runOnUiThread(new Runnable() {
             @Override
@@ -88,20 +92,31 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         });
 
 
-
     }
 
-
+*/
     @Test
     public void testMenu() {
        assertNotNull(navigationView);
+        mMainActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                drawerLayout.openDrawer(navigationView);
 
-       MenuItem challenges = navigationView.getMenu().findItem(R.id.challenges);
+            }
+        });
 
-       assertNotNull(challenges.isChecked());
-        mMainActivity.onOptionsItemSelected(challenges);
+
+        Menu menu = navigationView.getMenu();
+        MenuItem challenge = menu.findItem(R.id.challenges);
+        solo.clickOnActionBarItem(R.id.challenges);
+
+
+
 
     }
+
+
 
 
 
@@ -111,3 +126,4 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 
 
 }
+
