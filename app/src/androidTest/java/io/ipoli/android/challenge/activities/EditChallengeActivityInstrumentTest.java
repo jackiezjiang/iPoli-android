d47a88e63c2e6e462b363466d3f0ecba34afa75a
jackiezjiang <jackiezjiang@gmail.com>
2016-11-20 22:24:44 -0500

    Update EditChallengeActivityInstrumentTest.java

--- app/src/androidTest/java/io/ipoli/android/challenge/activities/EditChallengeActivityInstrumentTest.java
+++ app/src/androidTest/java/io/ipoli/android/challenge/activities/EditChallengeActivityInstrumentTest.java
@@ -1,16 +1,24 @@
 package io.ipoli.android.challenge.activities;
 
+import android.app.Fragment;
 import android.app.Instrumentation;
 import android.support.design.widget.TextInputEditText;
+import android.support.v7.view.menu.ActionMenuItemView;
 import android.support.v7.widget.Toolbar;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 import org.junit.Test;
 
 import io.ipoli.android.R;
 import io.ipoli.android.app.ui.CategoryView;
+import io.ipoli.android.challenge.ui.dialogs.MultiTextPickerFragment;
+import io.ipoli.android.quest.ui.dialogs.TextPickerFragment;
 
 /**
  * Created by YZ on 11/13/16.
@@ -57,7 +65,36 @@ public class EditChallengeActivityInstrumentTest extends ActivityInstrumentation
         CategoryView categoryView = (CategoryView) editChallengeActivity.findViewById(R.id.challenge_category);
         ImageView imageView = (ImageView) categoryView.findViewById(R.id.category_personal);
 
+        RelativeLayout relativeLayout = (RelativeLayout)  editChallengeActivity.findViewById(R.id.challenge_expected_results_container);
 
+        editChallengeActivity.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                relativeLayout.performClick();
+            }
+
+
+        });
+
+
+        TextView challengeQuestion = (TextView) editChallengeActivity.findViewById(R.id.challenge_expected_results_question);
+        assertNotNull(challengeQuestion.getText().toString());
+
+
+
+        ActionMenuItemView save=  (ActionMenuItemView) editChallengeActivity.findViewById(R.id.action_save);
+        assertNotNull(save);
+        editChallengeActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+                save.performClick();
+
+
+
+            }
+        });
 
 
 
